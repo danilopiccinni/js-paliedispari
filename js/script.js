d813991f8cce1509645391b910a14e19c2bea1fb
danilopiccinni <danilopiccinni@gmail.com>
2023-03-02 13:08:49 +0100

    Inizio : struttura iniziale esercizio
    collegamenti elementi in pagina pronti

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+Palidroma
+-Chiedere all’utente di inserire una parola
+-creare variabili per l'input e il bottone controllo
+-Creare una funzione per capire se la parola inserita è palindroma
+
+
+Pari e Dispari
+-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+-Sommiamo i due numeri
+-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+-Dichiariamo chi ha vinto.
+*/
+
+let inputParolaEl = document.getElementById ('input-parola')
+let bottoneContrParolaEl = document.getElementById ('bott-controllo-parola')
+let outputParolaEl = getElementById ('riultato-controllo-parola')
+
+let inputNumeroEl = document.getElementById ('input-numero')
+let bottoneContrNumeroEl = document.getElementById ('bottone-controllo-numero')
+let outputNumeroEl = getElementById ('riultato-controllo-numero')
